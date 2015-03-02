02cc28f6c52c5107bc54136a7c6be4d3ec028a37
mjhd <mjhd@devlion.net>
2015-03-02 20:07:48 +0900

    デーモンか

--- hsproom.go
+++ hsproom.go
@@ -1,10 +1,20 @@
 package main
 
-import "hsproom/gum"
+import (
+	"os"
+	"hsproom/gum"
+	"hsproom/utils"
+)
 
 func main() {
 
 	defer gum.Del()
+
+	err := gum.Daemonize()
+
+	if err != nil {
+		utils.PromulgateFatal(os.Stdout, err)
+	}
 
 	gum.Start()
 
