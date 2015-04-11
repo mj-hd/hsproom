3b99b83dd080b6a91cf273628bef0c14fa6f2229
mjhd <mjhd@devlion.net>
2015-04-11 23:56:02 +0900

    ヘルプをMarkdown記法に変更

--- controllers/help.go
+++ controllers/help.go
@@ -3,11 +3,20 @@ package controllers
 import (
 	"net/http"
 	"os"
+	"io/ioutil"
+	"html/template"
 
 	"hsproom/config"
 	"hsproom/templates"
 	"hsproom/utils/log"
+
+	"github.com/russross/blackfriday"
 )
+
+type helpMember struct {
+	*templates.DefaultMember
+	HelpContent template.HTML
+}
 
 func helpHandler(document http.ResponseWriter, request *http.Request) {
 
@@ -15,9 +24,24 @@ func helpHandler(document http.ResponseWriter, request *http.Request) {
 	tmpl.Layout = "default.tmpl"
 	tmpl.Template = "help.tmpl"
 
-	err := tmpl.Render(document, &templates.DefaultMember{
-		Title: "ヘルプ - " + config.SiteTitle,
-		User:  getSessionUser(request),
+	mdFile := config.TemplatesPath + "help/main.md"
+
+	mdRaw, err := ioutil.ReadFile(mdFile)
+	if err != nil {
+		log.Fatal(os.Stdout, err)
+
+		showError(document, request, "エラーが発生しました。")
+		return
+	}
+
+	markdown := blackfriday.MarkdownCommon(mdRaw)
+
+	err = tmpl.Render(document, helpMember{
+		DefaultMember: &templates.DefaultMember{
+			Title: "ヘルプ - " + config.SiteTitle,
+			User:  getSessionUser(request),
+		},
+		HelpContent: template.HTML(markdown),
 	})
 
 	if err != nil {
